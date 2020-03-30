6106fc4e388ab1a94e8a0298fb763bbca3d0b95c
Sharmin Alam Nipu <nipusharmin41@gmail.com>
2020-03-30 21:48:28 +0600

    with findViewByID coding

--- app/src/main/java/com/sharmin/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/sharmin/kotlinpractice/MainActivity.kt
@@ -2,16 +2,34 @@ package com.sharmin.kotlinpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+
+
+    lateinit var editTextName: EditText
+    lateinit var buttonClick:Button
+    lateinit var textViewShow:TextView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        toastMsg.setOnClickListener {
+         editTextName=findViewById(R.id.ETName)
+         buttonClick=findViewById(R.id.submitBtn)
+         textViewShow=findViewById(R.id.showText)
+
+         buttonClick.setOnClickListener(this)
+
+
+        /*  toastMsg.setOnClickListener {
               Toast.makeText(this,"Hello EveryOne",Toast.LENGTH_LONG).show()
           }
 
@@ -24,9 +42,12 @@ class MainActivity : AppCompatActivity() {
             showText.text= message
 
 
-        }
+        }  */
+    }
 
-
+    override fun onClick(v: View?) {
+        val name=editTextName.text.toString()
+        textViewShow.text="Hello $name"
 
     }
 }
